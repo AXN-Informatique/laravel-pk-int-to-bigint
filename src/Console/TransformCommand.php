20a479157d3d5ff12a5826780a462c26c1a63ff9
Vincent Garnier <forxer@gmail.com>
2022-11-09 19:06:10 +0100

    Drop Transformer injection from command constructor

--- src/Console/TransformCommand.php
+++ src/Console/TransformCommand.php
@@ -11,18 +11,9 @@ class TransformCommand extends Command
 
     protected $description = 'Convert DB primary keys and related foreign keys type from INT to BIGINT in a Laravel project';
 
-    protected $transformer;
-
-    public function __construct(Transformer $transformer)
+    public function handle(Transformer $transformer)
     {
-        $this->transformer = $transformer;
-        $this->transformer->setConsoleCommand($this);
-
-        parent::__construct();
-    }
-
-    public function handle()
-    {
-        $this->transformer->transform();
+        $transformer->setConsoleCommand($this);
+        $transformer->transform();
     }
 }
